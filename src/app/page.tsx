3c883c9fd6bf4d2fbdd581a22791456dac8bdfd8
jackon shiundu <jackonshiundu2019@gmail.com>
2023-04-04 00:08:13 +0300

    page-changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ import { groq } from 'next-sanity';
 import { client } from '../../lib/sanity.client';
 import Bloglist from '../../Components/Bloglist';
 const inter = Inter({ subsets: ['latin'] });
+export const revalidate = 60;
 
 const query = groq`*[_type=='post']{
   ...,
   author->,
   categories[]->
 }|order(_createdAt desc)`;
-export const revalidate = 60;
 
 export default async function Home() {
   if (previewData()) {
